d6c2eeb731a3cefa104cc3b70732787340ff716e
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-09 18:55:07 -0600

    chore: configure linting and correct errors
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def calculate(data):
     if len(data) < 9:
         raise ValueError('List must contain nine numbers.')
@@ -7,12 +8,16 @@ def calculate(data):
     a = np.array(data).reshape(3, 3)
 
     stats = {
-      'mean': [a.mean(axis = 0).tolist(), a.mean(axis = 1).tolist(), a.mean()],
-      'variance': [a.var(axis = 0).tolist(), a.var(axis = 1).tolist(), a.var()],
-      'standard deviation': [a.std(axis = 0).tolist(), a.std(axis = 1).tolist(), a.std()],
-      'max': [a.max(axis = 0).tolist(), a.max(axis = 1).tolist(), a.max()],
-      'min': [a.min(axis = 0).tolist(), a.min(axis = 1).tolist(), a.min()],
-      'sum': [a.sum(axis = 0).tolist(), a.sum(axis = 1).tolist(), a.sum()],
+        'mean': [a.mean(axis=0).tolist(), a.mean(axis=1).tolist(), a.mean()],
+        'variance': [a.var(axis=0).tolist(), a.var(axis=1).tolist(), a.var()],
+        'standard deviation': [
+            a.std(axis=0).tolist(),
+            a.std(axis=1).tolist(),
+            a.std()
+        ],
+        'max': [a.max(axis=0).tolist(), a.max(axis=1).tolist(), a.max()],
+        'min': [a.min(axis=0).tolist(), a.min(axis=1).tolist(), a.min()],
+        'sum': [a.sum(axis=0).tolist(), a.sum(axis=1).tolist(), a.sum()],
     }
 
     return stats
